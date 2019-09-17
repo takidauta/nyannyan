0a427a7a66d5ef9ae8896173ebcd1b21162f6bd1
utatakida <nkc19.uta.takida@gmail.com>
2019-09-17 12:37:31 +0900

    option変更 にゃこきちボタン配置途中

--- Assets/script/OputionButtonCon.cs
+++ Assets/script/OputionButtonCon.cs
@@ -9,6 +9,7 @@ public class OputionButtonCon : MonoBehaviour
     public void OnClick()
     {
         SceneManager.LoadScene("optionScene");
+        Debug.Log("giahdi");
     }
     // Start is called before the first frame update
     void Start()
