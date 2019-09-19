577e253b7420a255c75810d1f78cce67408f0fac
utatakida <nkc19.uta.takida@gmail.com>
2019-09-19 14:44:42 +0900

    練習に送ってみたにゃー

--- Assets/script/kingakuCon.cs
+++ Assets/script/kingakuCon.cs
@@ -6,22 +6,22 @@ using UnityEngine.UI;
 public class kingakuCon : MonoBehaviour
 {
     GameObject kingakuText;
-    float a;//現在の金額
+    float kingaku;//現在の金額
     int saidai;//金額の最大値
 
     // Start is called before the first frame update
     void Start()
     {
         this.kingakuText = GameObject.Find("KingakuText");
-        a = 0;
+        kingaku = 0;//金額を0から始める
         saidai = 2000;//金額の最大値はここで変更
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(a<saidai)//金額の最大値がsaidaiの値
-        a += Time.deltaTime*100;//1秒間に100円ずつ増える
-        this.kingakuText.GetComponent<Text>().text = a.ToString("F2")+"/"+saidai;
+        if(kingaku<saidai)//金額の最大値がsaidaiの値
+        kingaku += Time.deltaTime*100;//1秒間に100円ずつ増える
+        this.kingakuText.GetComponent<Text>().text = ((int)kingaku)+"/"+saidai;//現在の金額と最大値の金額をTextに表示
     }
 }
