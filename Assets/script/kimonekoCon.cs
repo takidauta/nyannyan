eefd2595943f59572ae47d490da1a7e2a4e17753
utatakida <nkc19.uta.takida@gmail.com>
2019-09-19 12:23:02 +0900

    戦闘シーン（骨組み）

---
+++ Assets/script/kimonekoCon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kimonekoCon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnClick()
+    {
+        Debug.Log("きもねこ出陣");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/script/kyosinnekoCon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kyosinnekoCon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnClick()
+    {
+        Debug.Log("巨神ねこ出陣");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
